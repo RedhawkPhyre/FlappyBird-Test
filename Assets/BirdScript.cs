480af5027d1dec0ed7f530e1b50a68d55a4b1ad2
RedhawkPhyre <shiraphyre@gmail.com>
2024-01-09 22:17:26 -0700

    Flappy Bird Base Mechanics
    Create collision with pipes
    Keep player score
    Set screen boundaries
    Game over screen
    Allow Replay

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -6,22 +6,34 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) == true)
+        if(Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive == true)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
 
+        if(transform.position.y > 11.2 || transform.position.y < -11)
+        {
+            birdIsAlive = false;
+            logic.gameOver();
+        }
 
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
